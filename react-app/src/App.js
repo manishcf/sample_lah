8f45e390c28bf416980f78d38b5423a9be069650
Amit Awinash <amitawinash@gmail.com>
2021-04-05 14:36:10 +0530

    A webpage with fields for Full name, email and message and a submit button

---
+++ react-app/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import UserQuery from "./UserQuery";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function CenteredGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={3}
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item xs={9}>
+          <Paper className={classes.paper}>
+            <UserQuery />
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
